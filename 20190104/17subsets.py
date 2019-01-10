dda16747da149df0f9e8bea278399779c283c2c2
PorryChen <porrychen@gmail.com>
2019-01-09 20:51:25 -0600

    update 17 add method 2

--- 20190104/17subsets.py
+++ 20190104/17subsets.py
@@ -5,18 +5,38 @@ class Solution:
     """
     def subsets(self, nums):
         # write your code here
-        self.result = []
-        self.search(sorted(nums), [], 0)
 
-        return self.result
+        # method 2
+        result = []
+        if nums is None:
+            return result
 
-    def search(self, nums, subset, index):
-        if index == len(nums):
-            self.result.append(list(subset))
-            return
+        nums.sort()
+        self.dfs(nums, 0, [], result)
 
-        subset.append(nums[index])
-        self.search(nums, subset, index + 1)
-        # Backtracking
-        subset.pop()
-        self.search(nums, subset, index + 1)
+        return result
+
+    def dfs(self, nums, startIndex, subset, result):
+        result.append(list(subset))
+
+        for i in range(startIndex, len(nums)):
+            subset.append(nums[i])
+            self.dfs(nums, i + 1, subset, result)
+            subset.pop()
+
+    #     # method 1
+    #     self.result = []
+    #     self.search(sorted(nums), [], 0)
+
+    #     return self.result
+
+    # def search(self, nums, subset, index):
+    #     if index == len(nums):
+    #         self.result.append(list(subset))
+    #         return
+
+    #     subset.append(nums[index])
+    #     self.search(nums, subset, index + 1)
+    #     # Backtracking
+    #     subset.pop()
+    #     self.search(nums, subset, index + 1)
