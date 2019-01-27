25533417b3baeea899cf5317c7f553b2f1d8b0fd
PorryChen <porrychen@gmail.com>
2019-01-27 15:48:28 -0600

    update 544. Top k Largest Numbers added two methods

--- 8 - Data Structure - Stack, Queue, Hash, Heap/544topkLargestNumbers.py
+++ 8 - Data Structure - Stack, Queue, Hash, Heap/544topkLargestNumbers.py
@@ -1,5 +1,4 @@
-import heapq
-
+# method 3
 class Solution:
     """
     @param nums: an integer array
@@ -8,10 +7,67 @@ class Solution:
     """
     def topk(self, nums, k):
         # write your code here
-        results, heap = [], []
-        for num in nums:
-            heapq.heappush(heap, (-num))
+        self.quick_sort(nums, 0, len(nums) - 1, k)
 
+        results = []
         for i in range(k):
-            results.append(-heapq.heappop(heap))
+            results.append(nums[i])
         return results
+
+    def quick_sort(self, nums, start, end, k):
+        if start >= k or start >= end:
+            return
+
+        left, right = start, end
+        pivot = nums[(start + end) // 2]
+
+        while left <= right:
+            while left <= right and nums[left] > pivot:
+                left += 1
+            while left <= right and nums[right] < pivot:
+                right -= 1
+
+            if left > right:
+                continue
+
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+        self.quick_sort(nums, start, right, k)
+        self.quick_sort(nums, left, end, k)
+
+# # method 2
+# class Solution:
+#     """
+#     @param nums: an integer array
+#     @param k: An integer
+#     @return: the top k largest numbers in array
+#     """
+#     def topk(self, nums, k):
+#         # write your code here
+#         heapq.heapify(nums)
+
+#         results = heapq.nlargest(k, nums)
+#         results.sort(reverse = True)
+
+#         return results
+
+# # method 1, fastest
+# import heapq
+
+# class Solution:
+#     """
+#     @param nums: an integer array
+#     @param k: An integer
+#     @return: the top k largest numbers in array
+#     """
+#     def topk(self, nums, k):
+#         # write your code here
+#         results, heap = [], []
+#         for num in nums:
+#             heapq.heappush(heap, (-num))
+
+#         for i in range(k):
+#             results.append(-heapq.heappop(heap))
+#         return results
