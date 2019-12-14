07bedea484f5e73f48252d389e28f03e9bd2fdd8
PorryChen <porrychen@gmail.com>
2019-12-13 18:39:32 -0600

    add 1324 java version

---
+++ Coding Interviews/01 - Interview Flow/E1324.CountPrimes.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /**
+     * @param n: a integer
+     * @return: return a integer
+     */
+    public int countPrimes(int n) {
+        // write your code here
+        if (n < 2) return 0;
+
+        int count = 0;
+        boolean[] notPrime = new boolean[n];
+
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i]) continue;
+
+            count++;
+            for (int j = 2; i * j < n; j++) {
+                notPrime[i * j] = true;
+
+            }
+        }
+
+        return count;
+    }
+}
