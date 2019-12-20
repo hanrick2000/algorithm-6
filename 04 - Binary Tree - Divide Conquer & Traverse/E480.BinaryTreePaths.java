d48471b870cb916325a6c6a3cbfc5279bc62763e
PorryChen <porrychen@gmail.com>
2019-12-19 18:24:31 -0600

    add 480.BinaryTreePaths java version

---
+++ 04 - Binary Tree - Divide Conquer & Traverse/E480.BinaryTreePaths.java
@@ -0,0 +1,41 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of the binary tree
+     * @return: all root-to-leaf paths
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        // write your code here
+        List<String> res = new ArrayList<String>();
+        if (root == null) return res;
+
+        helper(root, String.valueOf(root.val), res);
+        return res;
+    }
+
+    private void helper(TreeNode root, String path, List<String> res) {
+        if (root.left == null && root.right == null) {
+            res.add(path);
+            return;
+        }
+
+        if (root.left != null) {
+            helper(root.left, path + "->" + String.valueOf(root.left.val), res);
+        }
+
+        if (root.right != null) {
+            helper(root.right, path + "->" + String.valueOf(root.right.val), res);
+        }
+    }
+}
