67d8b3969a73b8c13e71ec5c621db11b5a01de65
PorryChen <porrychen@gmail.com>
2020-01-10 22:11:22 -0600

    add 155 and 97 java version

---
+++ 04 - Binary Tree - Divide Conquer & Traverse/E155.MinimumDepthofBinaryTree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree
+     * @return: An integer
+     */
+    public int minDepth(TreeNode root) {
+        // write your code here
+        if (root == null) return 0;
+
+        if (root.left == null || root.right == null) {
+            return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
